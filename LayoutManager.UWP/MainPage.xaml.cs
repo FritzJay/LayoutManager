0782a24a1f5bdf5614ed6c34873c83013ae3e17b
Fritz Jay <Jonathan.jay10@gmail.com>
2019-07-08 02:37:35 -0700

    Updated target platform version so I can work on my laptop. Added remove button logic. Hide remove button on items that are not selected.

--- LayoutManager.UWP/MainPage.xaml.cs
+++ LayoutManager.UWP/MainPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using LayoutManager.Model;
+using System;
 using System.Linq;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -37,7 +39,7 @@ namespace LayoutManager.UWP
             }
         }
 
-        private void Remove_Click(object sender, RoutedEventArgs e)
+        private void RemoveButton_Clicked(object sender, RoutedEventArgs e)
         {
             Button button = sender as Button;
             Layout layout = button.DataContext as Layout;
@@ -49,6 +51,55 @@ namespace LayoutManager.UWP
 
                 Layouts.ItemsSource = db.Layouts.ToList();
             }
+        }
+
+        private void Layouts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            GridView grid = sender as GridView;
+
+            foreach (var item in e.AddedItems)
+            {
+                var container = grid.ContainerFromItem(item) as FrameworkElement;
+                var button = FindControl<Button>(container, typeof(Button), "RemoveButton");
+                if (button != null)
+                {
+                    button.Visibility = Visibility.Visible;
+                }
+            }
+
+            foreach (var item in e.RemovedItems)
+            {
+                var container = grid.ContainerFromItem(item) as FrameworkElement;
+                var button = FindControl<Button>(container, typeof(Button), "RemoveButton");
+                if (button != null)
+                {
+                    button.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+
+        public static T FindControl<T>(UIElement parent, Type targetType, string ControlName) where T : FrameworkElement
+        {
+
+            if (parent == null) return null;
+
+            if (parent.GetType() == targetType && ((T)parent).Name == ControlName)
+            {
+                return (T)parent;
+            }
+            T result = null;
+            int count = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < count; i++)
+            {
+                UIElement child = (UIElement)VisualTreeHelper.GetChild(parent, i);
+
+                if (FindControl<T>(child, targetType, ControlName) != null)
+                {
+                    result = FindControl<T>(child, targetType, ControlName);
+                    break;
+                }
+            }
+            return result;
         }
     }
 }
